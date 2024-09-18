5448c889c47d7af98cfe93af65542b17f15832ab
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2024-09-18 08:45:29 +0530

    build: format and lint files

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from '@/client/providers/theme-provider';
-import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { Fragment } from 'react/jsx-runtime';
 
 export const Route = createRootRoute({
