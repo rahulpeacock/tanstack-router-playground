4586c1faa10428c352abf332cad0526ed017a4ce
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2024-12-22 18:52:48 +0530

    feat: add dev-tools to tanstack router

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,28 @@
 import { ThemeProvider } from '@/client/providers/theme-provider';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
+import React from 'react';
 import { Fragment } from 'react/jsx-runtime';
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
 
 export const Route = createRootRoute({
   component: () => (
     <Fragment>
       <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
         <Outlet />
+        <React.Suspense fallback={'🏝️'}>
+          <TanStackRouterDevtools />
+        </React.Suspense>
       </ThemeProvider>
     </Fragment>
   ),
